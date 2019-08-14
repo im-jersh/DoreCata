5a4201b55e92b7106428cf472b135ac9af77061c
oriyentel <joshuaosteen@me.com>
2019-08-13 21:57:58 -0600

    Initial Commit

---
+++ DoreCata/DoreCata.h
@@ -0,0 +1,19 @@
+//
+//  DoreCata.h
+//  DoreCata
+//
+//  Created by Joshua O'Steen on 8/13/19.
+//  Copyright © 2019 Random Order. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DoreCata.
+FOUNDATION_EXPORT double DoreCataVersionNumber;
+
+//! Project version string for DoreCata.
+FOUNDATION_EXPORT const unsigned char DoreCataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DoreCata/PublicHeader.h>
+
+
