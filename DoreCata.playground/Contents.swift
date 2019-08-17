eda609f6305a1c502d8051aa8a56f5d55da91773
oriyentel <joshuaosteen@me.com>
2019-08-17 15:57:41 -0600

    no changes here silly playground

--- DoreCata.playground/Contents.swift
+++ DoreCata.playground/Contents.swift
@@ -1,10 +1,1 @@
-import DoreCata
-
-struct TestCoreDataConfigurable: CoreDataConfigurable { }
-
-//let config = TestCoreDataConfigurable()
-//config.modelName
-//config.storeName
-//config.managedObjectModelURL
-//config.persistentStoreType
 
